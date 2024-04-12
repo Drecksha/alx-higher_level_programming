ffb40f407ff8c3d9dc4d1d8b15539d50fc221ee2
Drecksha <smashava91@gmail.com>
2024-04-12 06:05:53 -0700

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+90% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for US$1.99 $0 for 1 month.
+#!/usr/bin/python3
+"""
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
